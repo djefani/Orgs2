771090690a73214979e3578d59c695a0bb8b62df
djefani <djefani.eveeleen@gmail.com>
2023-11-28 10:45:36 -0300

    adicionando fontes e configurando tamanhos

--- app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs.dao
 
 import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,7 +14,12 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uvas",
+                valor = BigDecimal("15.99")
+            )
+        )
     }
 
 }
