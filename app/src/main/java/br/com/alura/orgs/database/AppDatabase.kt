6bbb4b9013c3bb7a2dbc5d8062303f00b1fad749
djefani <djefani.eveeleen@gmail.com>
2023-12-05 11:56:55 -0300

    adicionando banco de dados com o Room

---
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -0,0 +1,7 @@
+package br.com.alura.orgs.database
+
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase: RoomDatabase() {
+
+}
--- app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -2,10 +2,14 @@ package br.com.alura.orgs.model
 
 import java.math.BigDecimal
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity
 @Parcelize
 data class Produto(
+        @PrimaryKey(autoGenerate = true) val id: Long = 0L,
         val nome: String,
         val descricao: String,
         val valor: BigDecimal,
